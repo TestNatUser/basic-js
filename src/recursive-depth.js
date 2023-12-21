c66a9afebfa5cc1d5abf4517740f768d92730fe2
Natalia Furmanova <nfurmanova@tula.co>
2023-12-21 03:57:54 +0300

    feat: sum of digits

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if(!Array.isArray(arr)) {
+      return 0;
+    }
+    return 1 + arr.reduce((depth, el) => Math.max(depth, new DepthCalculator().calculateDepth(el)), 0);
   }
 }
 
